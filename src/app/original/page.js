35858e2e13f0f126f21b992df91ea9140f55d64b
Gustavo0623 <gustavo.0623@outlook.com>
2024-11-21 07:17:31 -0800

    add deck create function

--- src/app/original/page.js
+++ src/app/original/page.js
@@ -1,9 +1,24 @@
 import Cards from "../cards"
 export default function OriginalBlackjack () {
+    const cardTypes = ['spades', 'clubs', 'diamonds', 'hearts']
+    const cardSymbols = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    const deck = []
+    cardTypes.forEach(type => {
+        cardSymbols.forEach(symbol => {
+            const card = {
+                cardType : type,
+                cardSymbol : symbol
+            }
+            deck.push(card)
+        })        
+    });
+    console.log(deck)
     return (
         <main>
             <h1>Double Deck Blackjack</h1>
-            <Cards type="spades" symbol="K"/>
+            {deck.map((bjCard, cardIndex) => (
+                <Cards type={bjCard.cardType} symbol={bjCard.cardSymbol} key={cardIndex}/>
+            ))}
         </main>
     )
 }
